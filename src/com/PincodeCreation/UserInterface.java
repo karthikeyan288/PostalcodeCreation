c937e99fa85059543d7c2f00b9c2077c6cfc47e5
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-24 23:05:30 +0530

    ADDED : user information for pincode rule1

---
+++ src/com/PincodeCreation/UserInterface.java
@@ -0,0 +1,23 @@
+package com.PincodeCreation;
+
+import java.util.Scanner;
+
+public class UserInterface {
+	public static void main(String[] args) {
+		PincodePattern pincode = new PincodePattern();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the condition for 1:UC1 2:UC2");
+		int condition = sc.nextInt();
+		switch (condition) {
+		case 1:
+			pincode.pincodeUc1();
+			break;
+		case 2:
+			pincode.pincodeUc2();
+			break;
+		default:
+			System.out.println("Enter the valid data");
+			break;
+		}
+	}
+}
